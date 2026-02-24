44945f2c385e5cf28f9dcf971539b03a0a11873e
Mithila-Gaddam <mithila1705d6@gmail.com>
2026-02-23 19:04:57 -0500

    Bootstrapping of FlexiStock

---
+++ src/main/java/flexistock/HealthController.java
@@ -0,0 +1,12 @@
+package flexistock;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String index() {
+        return "FlexiStock is running";
+    }
+}
